dd296a14591ab5d352df33095856590aa99bf07c
GoForceX <goforcex@outlook.com>
2023-12-25 22:24:42 +0800

    Temporary announce

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,80 +6,78 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '出分啦',
-  tagline: '让卷王省下算分时间，专心开卷（误',
-  url: 'https://matrix.bjbybbs.com/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/died.ico',
-  organizationName: 'GoForceX', // Usually your GitHub org/user name.
-  projectName: 'bjbybbs-matrix', // Usually your repo name.
-  themes: [
-    [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
-      {
-        hashed: true,
-        language: ['zh', 'en'],
-        translations: {
-          "search_placeholder": "搜索",
-          "see_all_results": "查看所有结果",
-          "no_results": "没有匹配的结果",
-          "search_results_for": "\"{{ keyword }}\" 的全部搜索结果",
-          "search_the_documentation": "在文档中查找",
-          "count_documents_found": "找到了 {{ count }} 篇文章",
-          "no_documents_were_found": "没有匹配的文档",
-        },
-      }
-    ],
-  ],
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
-  plugins: [
-    'docusaurus-plugin-matomo',
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: '出分啦',
-        logo: {
-          alt: '寄！',
-          src: 'img/died.jpg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'landing',
-            position: 'left',
-            label: '帮助页',
-          },
-          // {to: '/blog', label: 'Blog', position: 'left'},
-          // {
-          //   href: 'https://github.com/facebook/docusaurus',
-          //   label: 'GitHub',
-          //   position: 'right',
-          // },
+    title: '出分啦',
+    tagline: '请注意，2024 年 1 月 10 日起，本软件将进入更新暂停期，不再提供新功能，仅提供 bug 修复。',
+    url: 'https://matrix.bjbybbs.com/',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/died.ico',
+    organizationName: 'GoForceX', // Usually your GitHub org/user name.
+    projectName: 'bjbybbs-matrix', // Usually your repo name.
+    themes: [
+        [
+            require.resolve('@easyops-cn/docusaurus-search-local'),
+            {
+                hashed: true,
+                language: ['zh', 'en'],
+                translations: {
+                    search_placeholder: '搜索',
+                    see_all_results: '查看所有结果',
+                    no_results: '没有匹配的结果',
+                    search_results_for: '"{{ keyword }}" 的全部搜索结果',
+                    search_the_documentation: '在文档中查找',
+                    count_documents_found: '找到了 {{ count }} 篇文章',
+                    no_documents_were_found: '没有匹配的文档',
+                },
+            },
         ],
-      },
-      footer: {
-        style: 'dark',
-        /*
+    ],
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
+    ],
+    plugins: ['docusaurus-plugin-matomo'],
+
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: '出分啦',
+                logo: {
+                    alt: '寄！',
+                    src: 'img/died.jpg',
+                },
+                items: [
+                    {
+                        type: 'doc',
+                        docId: 'landing',
+                        position: 'left',
+                        label: '帮助页',
+                    },
+                    // {to: '/blog', label: 'Blog', position: 'left'},
+                    // {
+                    //   href: 'https://github.com/facebook/docusaurus',
+                    //   label: 'GitHub',
+                    //   position: 'right',
+                    // },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                /*
         links: [
           {
             title: 'Docs',
@@ -122,19 +120,19 @@ const config = {
           },
         ],
         */
-        copyright: `Copyright © ${new Date().getFullYear()} GoForceX. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-      matomo: {
-        matomoUrl: 'https://matomo.goforcex.top/',
-        siteId: '4',
-        phpLoader: 'matomo.php',
-        jsLoader: 'matomo.js',
-      },
-    }),
+                copyright: `Copyright © ${new Date().getFullYear()} GoForceX. Built with Docusaurus.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+            matomo: {
+                matomoUrl: 'https://matomo.goforcex.top/',
+                siteId: '4',
+                phpLoader: 'matomo.php',
+                jsLoader: 'matomo.js',
+            },
+        }),
 };
 
 module.exports = config;
